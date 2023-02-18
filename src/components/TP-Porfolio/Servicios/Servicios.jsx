c2bf9fc30d69105278dcbd9e52482374ea362e9c
lcdiego <diegocolman78@hotmail.com>
2023-02-17 23:17:30 -0200

    cambio etiquetas

--- src/components/TP-Porfolio/Servicios/Servicios.jsx
+++ src/components/TP-Porfolio/Servicios/Servicios.jsx
@@ -10,18 +10,18 @@ const Servicios = () => {
         <div className='seccionServicios'>
             <div className='contenedorEtiqueas'>
                 <a className='iconoEtiquetas' href="#" ><FaPencilAlt/></a>
-                <h3 className='tituloEtiqueta'>ui/ui designacion</h3>
-                <p className='parrafoEtiqueta'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, reiciendis ad recusandae </p>
+                <h3 className='tituloEtiqueta'>Desarrollo web</h3>
+                <p className='parrafoEtiqueta'>Puedo diseñar cualquier tipo de interfaz  que me soliciten. Ya sea una Pág. Web, App, etc.</p>
             </div>
             <div className='contenedorEtiqueas'>
                 <a className='iconoEtiquetas' href="#" ><FaCode/></a>
-                <h3 className='tituloEtiqueta'>ui/ui designacion</h3>
-                <p className='parrafoEtiqueta'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, reiciendis ad recusandae </p>
+                <h3 className='tituloEtiqueta'>Desarrollo frontal</h3>
+                <p className='parrafoEtiqueta'>Puedo convertir tus diseños o bocetos dentro del marco Front-end.  </p>
             </div>
             <div className='contenedorEtiqueas'>
                 <a className='iconoEtiquetas' href="#" ><GiScarabBeetle/></a>
-                <h3 className='tituloEtiqueta'>ui/ui designacion</h3>
-                <p className='parrafoEtiqueta'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, reiciendis ad recusandae </p>
+                <h3 className='tituloEtiqueta'>Corrección de errores</h3>
+                <p className='parrafoEtiqueta'>Puedo resolver situaciones de emergencia y cualquier tipo de problemas que tengas en tu diseño.</p>
             </div>
             
 

02ef915c800cc29b2ca53994f6e79d04c6084f88
Lcdiego <diegocolman78@hotmail.com>
2023-08-01 19:29:06 -0300

    actializacion

--- src/components/TP-Porfolio/Main/Main.jsx
+++ src/components/TP-Porfolio/Main/Main.jsx
@@ -27,7 +27,7 @@ const Main= () => {
             <h1 className='titulo'>Acerca de mi</h1>
             <p className='parrafo'>Mi nombre es Diego Leandro Colman, tengo 44 años y vivo en la ciudad de  Plata , Buenos Aires, Argentina.</p>
             <p className='parrafo'> Soy programador Web Full Stack . Estudié en la Universidad Tecnológica Nacional (UTN) y mi prioridad es desempeñarme en diseños agradables y fáciles de utilizar para los usuarios.</p>
-            <p className='parrafo'>Tengo experiencia y referencias laborales comprobables.</p>
+            
             <p className='parrafo'>Me apasiona diseñar y programar para que mis clientes queden 100% satisfechos. Cada proyecto es un desafío personal, priorizando el crecimiento y buscando aumentar los ingresos de las empresas que me contraten.</p>
             <p className='parrafo'>En mi portfolio podrás observar detalladamente todas las tecnologías que utilizo al desarrollar mis proyectos y aquellos en lo que estoy trabajando en la actualidad.</p> 
             <p className='parrafo'>No dudes en consultarme. Estoy a tu disposición para responder todas tus dudas.</p>       

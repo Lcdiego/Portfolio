b8e8b2c396c2b82ea0aec84e2e2002b9501be204
Lcdiego <diegocolman78@hotmail.com>
2024-08-15 23:17:09 -0300

    actualizacion

--- src/components/Section-experiencia/Section.jsx
+++ src/components/Section-experiencia/Section.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../Section-experiencia/Section.css';
-import img_1 from './img/imagen_1.png'
-import img_2 from './img/imagen_2.png'
-import img_3 from './img/imagen_3.png'
-import img_4 from './img/imagen_4.png'
-import img_5 from './img/imagen_5.png'
-import img_6 from './img/imagen_6.png'
-import img_7 from './img/imagen_7.png'
-import img_8 from './img/imagen_8.png'
+import img_1 from './img/html.png'
+import img_2 from './img/css.png'
+import img_3 from './img/javascript.png'
+import img_4 from './img/react.png';
+import img_5 from './img/axios.png';
+import img_6 from './img/tailwind.png';
+import img_7 from './img/node.png'
+import img_8 from './img/api.png'
+import img_9 from './img/mongodb.png'
+import img_10 from './img/mysql.png'
 
 
 const Section = () => {
@@ -15,23 +17,50 @@ const Section = () => {
 
         <div className='contenedorSection' id='tecnologias'>
             <div className='contenedor_titulo_section'>
-                <h2  className='titulo_section'>Experiencia tecnologicas</h2>
+                <h2 className='titulo_section'>Experiencia tecnologicas</h2>
             </div>
 
             <h3 className='titulo_imagenes'>Frond end</h3>
             <div className='imagenes_tecnologias'>
+                <div className='seccion_imagen'>
+                    <img className='imagen' src={img_1} alt="" />
+                </div>
+                <div className='seccion_imagen'>
+                    <img className='imagen' src={img_2} alt="" />
+                </div>
+                <div className='seccion_imagen'>
+                    <img className='imagen' src={img_3} alt="" />
+                </div>
+                <div className='seccion_imagen'>
+                    <img className='imagen' src={img_4} alt="" />
+                </div>
+                <div className='seccion_imagen'>
+                    <img className='imagen' src={img_5} alt="" />
+                </div>
+                <div className='seccion_imagen'>
+                    <img className='imagen' src={img_6} alt="" />
+                </div>
 
-                <img className='seccion_imagen' src={img_1} alt="" />
-                <img className='seccion_imagen' src={img_2} alt="" />
-                <img className='seccion_imagen' src={img_3} alt="" />
-                <img className='seccion_imagen' src={img_4} alt="" />
             </div>
-            <h3 className='titulo_imagenes1'>Back end</h3>
+            <h3 className='titulo_imagenes'>Back end</h3>
             <div className='imagenes_tecnologias_1'>
-                <img className='seccion_imagen' src={img_5} alt="" />
-                <img className='seccion_imagen' src={img_6} alt="" />
-                <img className='seccion_imagen' src={img_7} alt="" />
-                <img className='seccion_imagen' src={img_8} alt="" />
+                <div className='seccion_imagen'>
+                    <img className='imagen' src={img_7} alt="" />
+                </div>
+                <div className='seccion_imagen'>
+                    <img className='imagen' src={img_8} alt="" />
+                </div>
+                <div className='seccion_imagen'>
+                    <img className='imagen' src={img_9} alt="" />
+                </div>
+                <div className='seccion_imagen'>
+                    <img className='imagen' src={img_10} alt="" />
+                </div>
+
+
+
+
+
             </div>
 
 
